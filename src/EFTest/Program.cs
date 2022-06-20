ee3fb354e35dc509be42212c5f920f5048de4c08
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-06-20 12:16:07 +0430

    EF random select

--- src/EFTest/Program.cs
+++ src/EFTest/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("Hello, World!");
+
+var dbContext = new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>());
+
+var randomBlogs = await dbContext.Blogs
+    .OrderBy(_ => Guid.NewGuid())
+    .Take(10)
+    .ToListAsync();
