4ed81ec1fd240d8393ef982aa6d38a856f7ab6b0
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-06-26 13:26:58 +0430

    add likes to blogs

--- src/Domain/Models/Blog.cs
+++ src/Domain/Models/Blog.cs
@@ -4,9 +4,11 @@ public class Blog : ModelBase
 {
     public string Title { get; set; }
 
-    public string Text { get; set; }
+    public string Text { get; init; }
 
     public long AuthorId { get; set; }
+
+    public int Likes { get; set; }
 
     public Author Author { get; set; }
 }
