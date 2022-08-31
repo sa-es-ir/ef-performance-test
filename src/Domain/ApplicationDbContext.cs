b6fb404475622567ec37a9de4fde9229ebd5e2a6
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2022-08-31 12:42:38 +0430

    use in-memory database

--- src/Domain/ApplicationDbContext.cs
+++ src/Domain/ApplicationDbContext.cs
@@ -5,16 +5,21 @@ namespace Domain;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-    {
+    //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    //{
 
+    //}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseInMemoryDatabase("TestDb");
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Blog>()
             .HasIndex(x => new { x.Title, x.Url })
-            .IsDescending(true, false);
+            .IsDescending(true, false);//<-- EF 7.0 new feature
     }
 
     public DbSet<Blog> Blogs { get; set; }
